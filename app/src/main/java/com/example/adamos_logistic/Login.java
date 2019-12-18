0a43b2006064bede8534216d9e3c29f2f6d96835
Alexey <leha-antipin2001@mail.ru>
2019-12-18 10:31:53 +0300

    Andrey sucked

--- app/src/main/java/com/example/adamos_logistic/Login.java
+++ app/src/main/java/com/example/adamos_logistic/Login.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class Login extends AppCompatActivity {
 
@@ -34,17 +42,38 @@ public class Login extends AppCompatActivity {
         View.OnClickListener login = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i;
-                switch (v.getId()) {
-                    case R.id.enter:
-                        i = new Intent(Login.this, Registration.class);
-                        startActivity(i);
-                        break;
-                    default:
-                        return;
-                }
-            }
+                try {
+                    URL url = new URL("http://192.168.137.1/adamos/hs/MAPI/test");
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    urlConnection.setRequestMethod("POST");
+                    urlConnection.setRequestProperty("Content-Type", "application/json; utf-8");
+                    urlConnection.setRequestProperty("Accept","application/json");
+                    urlConnection.setDoOutput(true);
+                    urlConnection.setDoInput(true);
+                    urlConnection.connect();
 
+                    JSONObject jsonParam = new JSONObject();
+                    jsonParam.put("e-mail", email.getText());
+                    jsonParam.put("password", password.getText().hashCode());
+
+                    DataOutputStream os = new DataOutputStream(urlConnection.getOutputStream());
+                    os.writeBytes(jsonParam.toString());
+
+                    os.flush();
+                    os.close();
+
+                    Log.i("STATUS", String.valueOf(urlConnection.getResponseCode()));
+                    Log.i("MSG" , urlConnection.getResponseMessage());
+
+                    urlConnection.disconnect();
+                }
+                catch(Exception e) {
+                    e.printStackTrace();
+                }
+                Intent i;
+                i = new Intent(Login.this, Registration.class);
+                startActivity(i);
+            }
         };
         registration.setOnClickListener(reg);
         enter.setOnClickListener(login);
--- app/src/main/java/com/example/adamos_logistic/Registration.java
+++ app/src/main/java/com/example/adamos_logistic/Registration.java
@@ -1,15 +1,24 @@
 package com.example.adamos_logistic;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONObject;
+
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import static android.hardware.camera2.params.RggbChannelVector.RED;
 
@@ -32,15 +41,50 @@ public class Registration extends AppCompatActivity {
         password = (EditText) findViewById(R.id.Password);
         PassRight = (EditText) findViewById(R.id.PassRight);
         registration = (Button) findViewById(R.id.registr) ;
+        final LinearLayout linear = (LinearLayout) findViewById(R.id.Linear);
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (v.getId() == R.id.registr && password.getText()!=PassRight.getText()) {
-                    check.setTextColor(RED);
-                    check.setText("Пароли не совпадают");
+                if (v.getId() == R.id.registr && !password.getText().toString().equals(PassRight.getText().toString())) {
+                    Toast.makeText(getApplicationContext(), "Andrey soset", Toast.LENGTH_LONG).show();
+                    //check.setTextColor(RED);
+                    //check.setText("Пароли не совпадают");
                 }
+                else if (v.getId() == R.id.registr) {
+                    try {
+                        URL urlRegistration = new URL("http://192.168.137.1/adamos/hs/MAPI/test");
+                        HttpURLConnection urlConRegistration = (HttpURLConnection) urlRegistration.openConnection();
+                        urlConRegistration.setRequestMethod("POST");
+                        urlConRegistration.setRequestProperty("Content-Type", "application/json; utf-8");
+                        urlConRegistration.setRequestProperty("Accept","application/json");
+                        urlConRegistration.setDoOutput(true);
+                        urlConRegistration.setDoInput(true);
+                        urlConRegistration.connect();
+
+                        JSONObject jsonParam = new JSONObject();
+                        jsonParam.put("e-mail", email.getText());
+                        jsonParam.put("password", password.getText().hashCode());
+                        jsonParam.put("surname", SurName.getText());
+                        jsonParam.put("second name", SecondName.getText());
+
+                        DataOutputStream os = new DataOutputStream(urlConRegistration.getOutputStream());
+                        os.writeBytes(jsonParam.toString());
+
+                        os.flush();
+                        os.close();
+
+                        Log.i("STATUS", String.valueOf(urlConRegistration.getResponseCode()));
+                        Log.i("MSG" , urlConRegistration.getResponseMessage());
+
+                        urlConRegistration.disconnect();
+                    }
+                    catch(Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                //Toast.makeText(getApplicationContext(), pidor, Toast.LENGTH_LONG).show();
             }
         };
-        //registration.setOnClickListener(listener);
+        registration.setOnClickListener(listener);
     }
 }
