b92fbb0d346422aafb78041bf68c11875cbd8fcf
Alexey <leha-antipin2001@mail.ru>
2020-01-09 14:36:40 +0300

    our chat

--- app/src/main/java/com/example/adamos_logistic/ChatActivity.java
+++ app/src/main/java/com/example/adamos_logistic/ChatActivity.java
@@ -1,14 +1,27 @@
 package com.example.adamos_logistic;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ChatActivity extends AppCompatActivity {
 
+    RecyclerView messageView;
+    EditText message;
+    ImageButton send;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat_activity);
+
+        //messageView = () findViewById (R.id.messages_view);
+        message = (EditText) findViewById(R.id.message);
     }
 }
