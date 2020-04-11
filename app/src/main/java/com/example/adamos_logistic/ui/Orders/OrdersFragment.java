cde45f715442d8168a292307fdf0cc6be731ba79
Alexey <leha-antipin2001@mail.ru>
2020-04-11 14:41:18 +0300

    GetRequestForOrders

--- app/src/main/java/com/example/adamos_logistic/ui/Orders/OrdersFragment.java
+++ app/src/main/java/com/example/adamos_logistic/ui/Orders/OrdersFragment.java
@@ -30,6 +30,8 @@ public class OrdersFragment extends Fragment {
 
     private JsonPlaceHolderApi jsonPlaceHolderApi;
 
+    private String api_key = "37bca7fce88fc16f0b666c64cc82cc54";
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_orders, container, false);
@@ -44,8 +46,9 @@ public class OrdersFragment extends Fragment {
         activeOrders.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                order.add(new Order("Активный заказ"));
-                recyclerView.setAdapter(adapter);
+                getHistoryOrders();
+                //order.add(new Order("Активный заказ"));
+                //recyclerView.setAdapter(adapter);
             }
         });
         return root;
@@ -60,29 +63,20 @@ public class OrdersFragment extends Fragment {
 
             jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
 
-            Call<GetResponseBodyOrders> call = jsonPlaceHolderApi.forOneOrder();
+            Call<List<GetResponseBodyOrders>> call = jsonPlaceHolderApi.getOrders(api_key);
 
-            call.enqueue(new Callback<GetResponseBodyOrders>() {
+            call.enqueue(new Callback<List<GetResponseBodyOrders>>() {
                 @Override
-                public void onResponse(Call<GetResponseBodyOrders> call, Response<GetResponseBodyOrders> response) {
+                public void onResponse(Call<List<GetResponseBodyOrders>> call, Response<List<GetResponseBodyOrders>> response) {
 
-                    List<GetResponseBodyOrders> getorders = (List<GetResponseBodyOrders>) response.body();
+                    List<GetResponseBodyOrders> getorders = response.body();
+                    Log.d("MyLog", "Запрос сформирован");
 
-                    for (GetResponseBodyOrders getOrder : getorders) {
-                        String content = "";
-                        content += "Order ID: " + getOrder.get_order_ID() + "\n";
-                        content += "Order info: " + getOrder.getOrderInfo() + "\n";
-                        content += "Date:: " + getOrder.get_date() + "\n";
-                        content += "Name: " + getOrder.get_name() + "\n\n";
-
-                        Log.d("MyLog", "ЗАПРОС СФОРМИРОВАН");
-
-                    }
                 }
 
                 @Override
-                public void onFailure(Call<GetResponseBodyOrders> call, Throwable t) {
-
+                public void onFailure(Call<List<GetResponseBodyOrders>> call, Throwable t) {
+                    Log.d("MyLog", "Запрос не сформирован");
                 }
             });
 
